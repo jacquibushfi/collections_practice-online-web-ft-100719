3f541a8dfe106a5d39717e2478553833a4f23d5f
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-09 18:47:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(integers)
+  integers.sort_by do |sort|
+    +sort
+  end
+end
+
+
+def sort_array_desc(integers)
+  integers.sort_by do |sort|
+    -sort
+  end
+end
+
+def sort_array_char_count(array)
+ array.sort do |a, b|
+  a.length <=> b.length
+ end
+end
+  
+  def swap_elements(array)
+     array[0], array[1], array[2] = array[0], array[2], array[1]
+  end
+    
+  def swap_elements_from_to(array, current, chosen)
+    array[current]= array[chosen]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+
+
+str[start,length] = str2
+
+
+
+
+
 
