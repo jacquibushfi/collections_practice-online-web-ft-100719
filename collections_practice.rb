83b5bfe7e2d65c249a138c4c3958c2c551e6f016
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-09 19:28:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,8 +54,7 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    if index != 1 
-      element = element + "s" 
+         element = element + "s"  if index != 1 
     end
   end
 end
